9e8cf7b11c545e7eda568203430de8fa0d0b89dc
Rayhan-446 <22235103446@cse.bubt.edu.bd>
2025-09-04 21:57:56 +0600

    basic calculation

---
+++ basic_calculation.dart
@@ -0,0 +1,14 @@
+void main() {
+  int num1 = 45;
+  int num2 = 23;
+
+  int sum = num1 + num2;
+  int sub = num1 - num2;
+  int mul = num1 * num2;
+  double div = num1 / num2;
+
+  print("sum = $sum");
+  print("Sub = $sub");
+  print("Mul = $mul");
+  print("Div = $div");
+}
