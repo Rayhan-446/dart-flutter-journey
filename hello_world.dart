7c3d8ad4fc449be6201cd24bf324343a61ca1399
Rayhan-446 <22235103446@cse.bubt.edu.bd>
2025-09-04 21:53:55 +0600

    first program added

--- hello_world.dart
+++ hello_world.dart
@@ -1,3 +1,9 @@
 void main() {
-  print("Hello world");
+  print("Hello world!");
+
+  var first_name = "Md";
+  var middle_name = "Abu";
+  var last_name = "Rayhan";
+
+  print("Hello Mr. $first_name $middle_name $last_name");
 }
